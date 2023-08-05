0175cf1100029f939f3e22190fbabb0b83253871
Cord Burmeister <Cord.Burmeister@live.de>
2023-08-05 14:29:13 +0200

    Update chocolatey with release notes

--- choco/UpdateChocoPackage.ps1
+++ choco/UpdateChocoPackage.ps1
@@ -3,9 +3,28 @@ $location = Get-Location
 
 Set-Location $PSScriptRoot
 
+
+if (-not [System.IO.Directory]::Exists("$PSScriptRoot\maiorumseries\bin"))
+{
+    [System.IO.Directory]::CreateDirectory("$PSScriptRoot\maiorumseries\bin")
+}
+
 Copy-Item -Path "$PSScriptRoot\..\src\gc2book\bin\Release\net6.0\*.*" -Destination "$PSScriptRoot\maiorumseries\bin" -Recurse
 
+if (-not [System.IO.Directory]::Exists("$PSScriptRoot\maiorumseries\docs"))
+{
+    [System.IO.Directory]::CreateDirectory("$PSScriptRoot\maiorumseries\docs")
+}
+Copy-Item -Path "$PSScriptRoot\..\ChangeLog.md" -Destination "$PSScriptRoot\maiorumseries\docs\ChangeLog.md"  -Force 
+
 Set-Location "$PSScriptRoot\maiorumseries"
+
+[xml]$nuspec = Get-Content "$PSScriptRoot\maiorumseries\maiorumseries.nuspec"   
+$changeLog =  Get-Content  "$PSScriptRoot\maiorumseries\docs\ChangeLog.md" | Out-String
+
+$nuspec.package.metadata.releaseNotes = $changeLog.ToString()
+$nuspec.Save("$PSScriptRoot\maiorumseries\maiorumseries.nuspec")
+#Set-Content $nuspec -Path "$PSScriptRoot\maiorumseries\maiorumseries.nuspec"
 
 choco pack 
 
